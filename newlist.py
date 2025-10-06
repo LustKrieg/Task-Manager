777a538f65269d6252f37762b664f837c7a4641b
Lust_Krieg <159123881+LustKrieg@users.noreply.github.com>
2025-10-06 18:57:07 +0300

    Update newlist.py

--- newlist.py
+++ newlist.py
@@ -7,5 +7,7 @@ from tkinter import *
 
 root = Tk()
 root.geometry("800x600")
+root.title("Roberto 0.1")
+root.resizable(False, False) # Prevents both horizontal and vertical resizing
 
-root.title 
+root.mainloop()
